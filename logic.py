874874698643c861146baeeb15d6f2369600807e
Alexei <alexalekov7@gmail.com>
2026-01-18 13:50:36 +0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -7,8 +7,6 @@ class Pokemon:
     def __init__(self, pokemon_trainer):
 
         self.pokemon_trainer = pokemon_trainer   
-
-
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
@@ -36,19 +34,43 @@ class Pokemon:
             return "Pikachu"
 
 
-
-
+    def plus_age(self,age=1):
+            plus = randint(1,3)
+            age += plus
+            # Определяем правильную форму слова
+            last_two_digits = age % 100
+            last_digit = age % 10
+            
+            if 11 <= last_two_digits <= 14:
+                form = "лет"
+            elif last_digit == 1:
+                form = "год"
+            elif last_digit in [2, 3, 4]:
+                form = "года"
+            else:
+                form = "лет"
+            return f"Ваш покемон вырос на {plus} {form}"\
+            f"\nему {age} {form}"
+            
 
 
 
     # Метод класса для получения информации 
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"\
-        f"\nНомер твоего покеомона: {self.pokemon_number}"\
+        if self.pokemon_number == 42 or self.pokemon_number ==666 or self.pokemon_number ==52 or self.pokemon_number ==67 or self.pokemon_number ==555 or self.pokemon_number ==444 or self.pokemon_number ==777 or self.pokemon_number ==888 or self.pokemon_number ==999 or self.pokemon_number ==1001:
+            return "Поздравляю вам попался редкий покемон:"\
+            f"Имя твоего покеомона: {self.name}"\
+            f"\nНомер твоего покеомона: {self.pokemon_number}"
+        else:
+            return f"Имя твоего покеомона: {self.name}"\
+            f"\nНомер твоего покеомона: {self.pokemon_number}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+
+
+
 
 
 
